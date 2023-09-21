91cf357ab3ba1f4d0cc81350a628bad769d44f26
karu1325 <143956677+karu1325@users.noreply.github.com>
2023-09-21 11:59:08 +0000

    [Add]4-14

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ root to: "homes#top"
 
 resources :post_images, only: [:new, :create, :index, :show, :destroy]
 get 'homes/about' => 'homes#about', as: 'about'
+resources :users, only: [:show, :edit]
 end
